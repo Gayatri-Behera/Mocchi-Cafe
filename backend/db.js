cacd0af590cbd385defa5cb48a8be2c0bbb0d671
Gayatri Behera <161240899+Gayatri-Behera@users.noreply.github.com>
2025-02-16 00:35:38 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,29 @@
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb+srv://username:password@projectname.mongodb.net/databasename?retryWrites=true&w=majority&appName=';
 
+const mongoDB = async() => {
+    await mongoose.connect(mongoURI).then(async() => {
+        console.log("Connected to database succesully!");
+        const coffeeItems = await mongoose.connection.db.collection("coffeeitems").find({}).toArray();
+        try {
+            global.coffeeItems = coffeeItems;
+            console.log(global.coffeeItems);
+            const coffeeCategory = await mongoose.connection.db.collection("coffeeCategory").find({}).toArray();
+            try {
+                global.coffeeCategory = coffeeCategory;
+                console.log(global.coffeeCategory);
+            }
+            catch(err) {
+                console.log("error", err);
+            }
+        }
+        catch(err) {
+            console.log("error", err);
+        }
+    }).catch(() => {
+        console.log("Database cannot be connected.Try again after sometime!");
+    })
+}
+
+
+module.exports = mongoDB;
